eb1e792e497563ceb98f45a7f1e22692383c7cce
putkidibosh <putkidibosh@gmail.com>
2026-05-04 08:30:33 +0600

    Delete commands/utility.py

--- commands/utility.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-
-def register(bot):
-    @bot.message_handler(commands=['refresh'])
-    def clear_cache(message):
-        files = glob.glob('cache/*')
-        for f in files: os.remove(f)
-        bot.reply_to(message, "🧹 𝐒𝐲𝐬𝐭𝐞𝐦 𝐂𝐥𝐞𝐚𝐧𝐞𝐝! ⚔️")
